4a040a45d20bdfc65e0e2e3af1253d14fb126952
martleru <68188509+martleru@users.noreply.github.com>
2024-07-05 13:53:44 +0300

    Updated API URL's

--- src/utils/api.ts
+++ src/utils/api.ts
@@ -1,4 +1,3 @@
-
 export const geoApiOptions = {
   method: "GET",
   headers: {
@@ -8,5 +7,5 @@ export const geoApiOptions = {
 };
 
 export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo";
-
-
+export const WEATHER_API_URL = "https://api.openweathermap.org/data/2.5";
+export const WEATHER_API_KEY = "4f0cf1ec050edcb8ad32ed066afc3dd5";
