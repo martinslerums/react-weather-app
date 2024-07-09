6218524a458be8ef48434328f2a4006c3054d45e
martleru <68188509+martleru@users.noreply.github.com>
2024-07-09 13:22:27 +0300

    Added routes component to better manage routes.

---
+++ src/components/routes/AppRoutes.tsx
@@ -0,0 +1,15 @@
+import { Routes, Route } from 'react-router-dom';
+import Location from '../../pages/Location';
+import Home from '../../pages/Home';
+
+
+const AppRoutes = () => {
+  return (
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/location/:name/:latitude/:longitude" element={<Location />} />
+    </Routes>
+  );
+};
+
+export default AppRoutes;
