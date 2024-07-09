4ed2ca48deae3827c74d1c65fe8628eb4cc5b4a7
martleru <68188509+martleru@users.noreply.github.com>
2024-07-09 13:17:40 +0300

    Added routes to use react-router-dom

--- src/App.tsx
+++ src/App.tsx
@@ -1,48 +1,9 @@
 import "./App.css";
 
-import { useState } from "react";
-
-import { WEATHER_API_URL, WEATHER_API_KEY } from "./utils/api";
-import { CityOption, ForecastData, WeatherData } from "./utils/typings";
-
-import Search from "./components/search/Search";
-import Forecast from "./components/forecast/Forecast";
-import CurrentWeather from "./components/current-weather/Current-weather";
+import AppRoutes from "./components/routes/AppRoutes";
 
 function App() {
-  const [currentWeather, setCurrentWeather] = useState<WeatherData | null>(
-    null
-  );
-  const [forecast, setForecast] = useState<ForecastData | null>(null);
-
-  const handleOnSearchChange = (searchData: CityOption) => {
-    const [lat, lon] = searchData.value.split(" ");
-
-    const currentWeatherFetch = fetch(
-      `${WEATHER_API_URL}/weather?lat=${lat}&lon=${lon}&appid=${WEATHER_API_KEY}&units=metric`
-    );
-    const forecastFetch = fetch(
-      `${WEATHER_API_URL}/forecast?lat=${lat}&lon=${lon}&appid=${WEATHER_API_KEY}&units=metric`
-    );
-
-    Promise.all([currentWeatherFetch, forecastFetch])
-      .then(async (response) => {
-        const weatherResponse = await response[0].json();
-        const forecastResponse = await response[1].json();
-
-        setCurrentWeather({ city: searchData.label, ...weatherResponse });
-        setForecast({ city: searchData.label, ...forecastResponse });
-      })
-      .catch((error) => console.error(error));
-  };
-
-  return (
-    <div className="container">
-      <Search onSearchChange={handleOnSearchChange} />
-      {currentWeather && <CurrentWeather currentWeatherData={currentWeather} />}
-      {forecast && <Forecast forecast={forecast} />}
-    </div>
-  );
+  return <AppRoutes />;
 }
 
 export default App;
