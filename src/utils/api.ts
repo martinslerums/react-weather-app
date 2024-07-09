41d78e9d3af214551126b49ff0cb0df6672637b7
martleru <68188509+martleru@users.noreply.github.com>
2024-07-09 13:18:34 +0300

    Using different API for Location select elements.

--- src/utils/api.ts
+++ src/utils/api.ts
@@ -1,11 +1,3 @@
-export const geoApiOptions = {
-  method: "GET",
-  headers: {
-    "x-rapidapi-key": "7221fdcf10mshca16ba09b780ec6p189a59jsn3f965b8f7386",
-    "x-rapidapi-host": "wft-geo-db.p.rapidapi.com",
-  },
-};
-
 export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo";
 export const WEATHER_API_URL = "https://api.openweathermap.org/data/2.5";
 export const WEATHER_API_KEY = "4f0cf1ec050edcb8ad32ed066afc3dd5";
